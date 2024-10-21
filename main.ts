c5879d5941d16425220ac78430675b3f8c33b172
Nguyen <tyson.tran2009@gmail.com>
2024-10-21 14:46:28 +1100

    Power Save changes along side new sprites

--- main.ts
+++ main.ts
@@ -2,10 +2,12 @@ enum RadioMessage {
     message1 = 49434,
     Connected$ = 11087
 }
+/**
+ * Power saving feature reduces comm frequency + led, slightly glitched
+ */
 input.onButtonPressed(Button.A, function () {
-    Status = 3
+    State = 3
     if (Status == 2) {
-        let P2T = 0
         basic.clearScreen()
         basic.showString("P2T")
         basic.pause(5000)
@@ -20,12 +22,39 @@ input.onButtonPressed(Button.A, function () {
         basic.pause(5000)
         basic.clearScreen()
     } else {
-        State += 1
-        basic.showNumber(input.temperature())
-        basic.pause(1000)
-        basic.clearScreen()
-        State = 0
+        if (input.temperature() > 29) {
+            for (let index = 0; index < 4; index++) {
+                basic.showLeds(`
+                    # . . # .
+                    # . # # #
+                    # . . # .
+                    # # . . .
+                    # . . . .
+                    `)
+                basic.pause(100)
+                basic.clearScreen()
+                basic.pause(100)
+            }
+        } else if (input.temperature() < 10) {
+            for (let index = 0; index < 4; index++) {
+                basic.showLeds(`
+                    # . . . .
+                    # . # # #
+                    # . . . .
+                    # # . . .
+                    # . . . .
+                    `)
+                basic.pause(100)
+                basic.clearScreen()
+                basic.pause(100)
+            }
+        } else {
+            basic.showNumber(input.temperature())
+            basic.pause(1000)
+            basic.clearScreen()
+        }
     }
+    State = 0
 })
 radio.onReceivedMessage(RadioMessage.Connected$, function () {
     Connection = 2
@@ -44,10 +73,8 @@ input.onButtonPressed(Button.AB, function () {
     State = 0
 })
 input.onButtonPressed(Button.B, function () {
-    Status = 3
+    State = 3
     if (Status == 2) {
-        let P3M = 0
-        let P3T = 0
         basic.clearScreen()
         basic.showString("P3T")
         basic.pause(5000)
@@ -62,10 +89,37 @@ input.onButtonPressed(Button.B, function () {
         basic.pause(5000)
         basic.clearScreen()
     } else {
-        State += 1
-        basic.showNumber(pins.analogReadPin(AnalogPin.P0))
-        basic.pause(1000)
-        basic.clearScreen()
+        if (pins.analogReadPin(AnalogPin.P0) > 600) {
+            for (let index = 0; index < 4; index++) {
+                basic.showLeds(`
+                    # # . . .
+                    # . . . #
+                    . . . # #
+                    . . # # #
+                    . . . . #
+                    `)
+                basic.pause(100)
+                basic.clearScreen()
+                basic.pause(100)
+            }
+        } else if (pins.analogReadPin(AnalogPin.P0) < 120) {
+            for (let index = 0; index < 4; index++) {
+                basic.showLeds(`
+                    . # . . .
+                    # # # . #
+                    . . . # #
+                    # . # # #
+                    . # . . #
+                    `)
+                basic.pause(100)
+                basic.clearScreen()
+                basic.pause(100)
+            }
+        } else {
+            basic.showNumber(pins.analogReadPin(AnalogPin.P0))
+            basic.pause(1000)
+            basic.clearScreen()
+        }
         State = 0
     }
 })
@@ -73,11 +127,11 @@ radio.onReceivedValue(function (name, value) {
     if (name == "P2M") {
         P2M = value
     } else if (name == "P2T") {
-        P2M = value
+        P2T = value
     } else if (name == "P3T") {
-        P2M = value
+        P3T = value
     } else if (name == "P3M") {
-        P2M = value
+        P3M = value
     } else {
     	
     }
@@ -146,13 +200,44 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
         State = 0
     }
 })
+let P3M = 0
+let P3T = 0
 let Connection = 0
-let State = 0
 let P2M = 0
+let P2T = 0
 let Status = 0
+let State = 0
 radio.setTransmitPower(7)
 radio.setGroup(1)
-basic.forever(function () {
+loops.everyInterval(10000, function () {
+    if (Connection > 0) {
+        Connection += -1
+    }
+})
+loops.everyInterval(5000, function () {
+    if (State == 0) {
+        basic.pause(9000)
+        if (State == 0) {
+            while (State == 0) {
+                led.setBrightness(44)
+                basic.showLeds(`
+                    . # . . .
+                    # # # . #
+                    . # . # .
+                    . . . . #
+                    . . . # .
+                    `)
+                basic.pause(5000)
+                basic.clearScreen()
+                basic.pause(5000)
+                Status = 10
+            }
+            led.setBrightness(255)
+            Status = 0
+        }
+    }
+})
+loops.everyInterval(5000, function () {
     if (Status == 10) {
         while (Status == 10) {
             basic.pause(30000)
@@ -168,31 +253,5 @@ basic.forever(function () {
         radio.sendValue("P1T", input.temperature())
         radio.sendValue("P1M", pins.analogReadPin(AnalogPin.P0))
         radio.sendMessage(RadioMessage.Connected$)
-    }
-})
-loops.everyInterval(5000, function () {
-    if (State == 0) {
-        basic.pause(9000)
-        if (State == 0) {
-            while (State == 0) {
-                led.setBrightness(97)
-                basic.showLeds(`
-                    . # . . .
-                    # # # . #
-                    . # . # .
-                    . . . . #
-                    . . . # .
-                    `)
-                basic.pause(500)
-                basic.clearScreen()
-                basic.pause(1000)
-                State = 10
-            }
-        }
-    }
-})
-loops.everyInterval(5000, function () {
-    if (Connection > 0) {
-        Connection += -1
     }
 })
